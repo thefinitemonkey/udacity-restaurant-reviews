93730f9cf4bb430d2777f261e9e4fd4ad381998c
Doug Brown <dougb@finitemonkey.com>
2018-04-30 21:36:28 -0500

    bug: Fix semantic tag for nav

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -155,6 +155,7 @@ fillBreadcrumb = (restaurant = self.restaurant) => {
   const breadcrumb = document.getElementById("breadcrumb");
   const li = document.createElement("li");
   li.innerHTML = restaurant.name;
+  li.ariaCurrent = "page";
   breadcrumb.appendChild(li);
 };
 
